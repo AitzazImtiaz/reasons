4340eb317c7d57ac3f8495fca4e44538b7554f8f
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-08-26 02:38:20 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,16 @@ to the programming language.
 
 int main(int argc, char *argv[]){
   // Default output
-  cout << ""\x1B[34mThis is Blue Text\033[0m\n"";
+  cout << ""\x1B[34mReasons,\033[0m\n"";
   cout << "v 0.0.0";
-  cout << "";
+  cout << "\x1B[92m by Aitzaz Imtiaz\033[0m\n";
+  cout << "\n";
+  cout << "Reasons first prototype was developed in late 2025 by Aitzaz Imtiaz under The Broke Consortium \n";
+  cout << "of Mathematical Research, aiming to study a discourse on logic, and other non-trivial mathematical \n";
+  cout << "fields to improvise upon philosophical developments. Much of its development took place around in \n";
+  cout << "reformation of Broke Quarterly and deepening the legislation of consortium allegiance.";
+  cout << "\n \n";
+  cout << "The language was inspired by K-drama \"When Life Gives You Tangerines\" and named after the American \n";
+  cout << "drama \"13 Reasons Why\" character Clay Jensen. This is why, Reasons support both Korean and English \n";
+  return 0;
 }
