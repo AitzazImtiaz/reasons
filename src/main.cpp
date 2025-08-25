340ef68372e55c3dbd777f3095c87cf61a372e92
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-08-25 14:38:34 +0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/* 
+Reasons entry point: The following file will handle all entry points
+to the programming language.
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[]){
+  // Default output
+  cout << ""\x1B[34mThis is Blue Text\033[0m\n"";
+  cout << "v 0.0.0";
+  cout << "";
+}
