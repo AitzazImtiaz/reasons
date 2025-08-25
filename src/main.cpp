50c821c8aec5c8848b99791b87c78dd48d20b39a
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-08-26 04:20:20 +0500

    fix: logical issues in arguments resolved

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,22 @@ to the programming language.
 #include "repl.h"
 #include "cxxopts.hpp"
 
+void intro(const std::string& version){
+  // Default output
+  std::cout << "\x1B[34mReasons,\033[0m";
+  std::cout << version << "\n";
+  std::cout << "\x1B[92m by Aitzaz Imtiaz\033[0m\n";
+  std::cout << "\n";
+  std::cout << "Reasons first prototype was developed in late 2025 by Aitzaz Imtiaz under The Broke Consortium \n";
+  std::cout << "of Mathematical Research, aiming to study a discourse on logic, and other non-trivial mathematical \n";
+  std::cout << "fields to improvise upon philosophical developments. Much of its development took place around in \n";
+  std::cout << "reformation of Broke Quarterly and deepening the legislation of consortium allegiance.";
+  std::cout << "\n \n";
+  std::cout << "The language was inspired by K-drama \"When Life Gives You Tangerines\" and named after the American \n";
+  std::cout << "drama \"13 Reasons Why\" character Clay Jensen. This is why, Reasons intend to support both Korean and \n";
+  std::cout << "English. \n";
+  std::cout << "\n Type \"--help\" for further help. \n";  
+}
 int main(int argc, char *argv[]){
   std::ifstream versionFile("../VERSION");
   if (!versionFile) {
@@ -20,20 +36,10 @@ int main(int argc, char *argv[]){
   std::getline(versionFile, version); // stores the result in version variable
   versionFile.close();
 
-  // Default output
-  std::cout << "\x1B[34mReasons,\033[0m\n";
-  std::cout << version;
-  std::cout << "\x1B[92m by Aitzaz Imtiaz\033[0m\n";
-  std::cout << "\n";
-  std::cout << "Reasons first prototype was developed in late 2025 by Aitzaz Imtiaz under The Broke Consortium \n";
-  std::cout << "of Mathematical Research, aiming to study a discourse on logic, and other non-trivial mathematical \n";
-  std::cout << "fields to improvise upon philosophical developments. Much of its development took place around in \n";
-  std::cout << "reformation of Broke Quarterly and deepening the legislation of consortium allegiance.";
-  std::cout << "\n \n";
-  std::cout << "The language was inspired by K-drama \"When Life Gives You Tangerines\" and named after the American \n";
-  std::cout << "drama \"13 Reasons Why\" character Clay Jensen. This is why, Reasons intend to support both Korean and \n";
-  std::cout << "English. \n";
-  std::cout << "\n Type \"--help\" for further help.";
+  if (argc < 2){
+    intro(version);
+    return 0;
+  }
 
   cxxopts::Options options("Reasons", "A philosophical programming language");
   options.add_options()
@@ -41,13 +47,20 @@ int main(int argc, char *argv[]){
     ("r,repl", "Run the REPL")
     ("v,version", "Show version info");
 
-  auto argument = options.parse(argc, argv);
+  cxxopts::ParseResult argument;
+  try {
+    argument = options.parse(argc, argv);
+  } catch (const cxxopts::OptionException& e) {
+    std::cerr << "Error parsing options: " << e.what() << std::endl;
+    return 1;
+  }
+
   if (argument.count("help")) {
     std::cout << options.help() << std::endl;
     return 0;
   }
   if (argument.count("version")){
-    std::cout << version;
+    std::cout << version << "\n";
     return 0;
   }
   if (argument.count("repl")) {
